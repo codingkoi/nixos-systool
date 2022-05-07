b5daf6aca592ca0b5b7648ee767e784401d51634
Jeremy Shoemaker <jeremy@codingkkoi.com>
2022-05-07 14:46:05 -0500

    nixos-systool: switch to `crane`
    I think `crane` does a better job of managing Rust projects, so I've
    switched to using it for `nixos-systool` for a start.

--- pkgs/tools/nixos-systool/flake.nix
+++ pkgs/tools/nixos-systool/flake.nix
@@ -1,7 +1,10 @@
 {
   inputs = {
-    naersk.url = "github:nmattia/naersk/master";
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+    crane = {
+      url = "github:ipetkov/crane";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
     utils.url = "github:numtide/flake-utils";
     flake-compat = {
       url = "github:edolstra/flake-compat";
@@ -9,17 +12,24 @@
     };
   };
 
-  outputs = { self, nixpkgs, naersk, utils, flake-compat }:
+  outputs = { self, nixpkgs, crane, utils, flake-compat }:
     utils.lib.eachDefaultSystem (system:
       let
         pkgs = import nixpkgs { inherit system; };
-        naersk-lib = pkgs.callPackage naersk {};
+        craneLib = crane.lib.${system};
+        src = ./.;
+        # Build *just* the cargo dependencies separately
+        # so we don't rebuild them every time the code changes
+        cargoArtifacts = craneLib.buildDepsOnly {
+          inherit src;
+        };
+
+        crate = craneLib.buildPackage {
+          inherit cargoArtifacts src;
+        };
       in {
         # `nix build`
-        defaultPackage = naersk-lib.buildPackage {
-          pname = "nixos-systool";
-          root = ./.;
-        };
+        packages.default = crate;
         # `nix run`
         defaultApp = utils.lib.mkApp {
             drv = self.defaultPackage."${system}";
@@ -28,12 +38,12 @@
         #
         # I'm using `rustup` here because Clion/Rust can't find the Rust stdlib
         # sources without it. I'm not sure what's going on there.
-        devShell = with pkgs; mkShell {
-          nativeBuildInputs = [ rustup rustfmt pre-commit rustPackages.clippy ];
+        devShells.default = with pkgs; mkShell {
+          nativeBuildInputs = [ cargoArtifacts rustc rust-analyzer cargo rustfmt clippy ];
           RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          shellHook = ''
-            ${rustup}/bin/rustup update
-          '';
+          # shellHook = ''
+          #   ${rustup}/bin/rustup update
+          # '';
         };
       });
 }
