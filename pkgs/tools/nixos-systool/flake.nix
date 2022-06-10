d29020fe47832594c487f902893df775a1c6441c
Jeremy Shoemaker <jeremy@codingkkoi.com>
2022-06-10 15:49:01 +0100

    Run nixfmt on everything... again

--- pkgs/tools/nixos-systool/flake.nix
+++ pkgs/tools/nixos-systool/flake.nix
@@ -20,30 +20,25 @@
         src = ./.;
         # Build *just* the cargo dependencies separately
         # so we don't rebuild them every time the code changes
-        cargoArtifacts = craneLib.buildDepsOnly {
-          inherit src;
-        };
+        cargoArtifacts = craneLib.buildDepsOnly { inherit src; };
 
-        crate = craneLib.buildPackage {
-          inherit cargoArtifacts src;
-        };
+        crate = craneLib.buildPackage { inherit cargoArtifacts src; };
       in {
         # `nix build`
         packages.default = crate;
         # `nix run`
-        defaultApp = utils.lib.mkApp {
-            drv = self.defaultPackage."${system}";
-        };
+        defaultApp = utils.lib.mkApp { drv = self.defaultPackage."${system}"; };
         # `nix develop`
         #
         # I'm using `rustup` here because Clion/Rust can't find the Rust stdlib
         # sources without it. I'm not sure what's going on there.
-        devShells.default = with pkgs; mkShell {
-          nativeBuildInputs = [ rustup rust-analyzer rustfmt clippy ];
-          RUST_SRC_PATH = rustPlatform.rustLibSrc;
-          shellHook = ''
-            ${rustup}/bin/rustup update
-          '';
-        };
+        devShells.default = with pkgs;
+          mkShell {
+            nativeBuildInputs = [ rustup rust-analyzer rustfmt clippy ];
+            RUST_SRC_PATH = rustPlatform.rustLibSrc;
+            shellHook = ''
+              ${rustup}/bin/rustup update
+            '';
+          };
       });
 }
