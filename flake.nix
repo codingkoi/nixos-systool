7bbee2c96e9e50ec1b7ace5ce3a611de5e5fb90c
Jeremy Shoemaker <jeremy@codingkoi.com>
2023-05-30 20:39:22 -0500

    Switch to using flake-parts
    Use flake-parts like I do in my other projects, and add a devshell configuration.

--- flake.nix
+++ flake.nix
@@ -5,62 +5,95 @@
       url = "github:nmattia/naersk/master";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-    utils.url = "github:numtide/flake-utils";
-    flake-compat = {
-      url = "github:edolstra/flake-compat";
-      flake = false;
+    flake-parts.url = "github:hercules-ci/flake-parts";
+    devshell = {
+      url = "github:numtide/devshell";
+      inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, naersk, utils, ... }:
-    utils.lib.eachDefaultSystem (system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-        naersk-lib = naersk.lib."${system}";
-        lists = pkgs.lib.lists;
-        strings = pkgs.lib.strings;
+  outputs = inputs@{ self, nixpkgs, naersk, flake-parts, devshell, ... }:
+    flake-parts.lib.mkFlake { inherit inputs; } ({
+      imports = [ devshell.flakeModule ];
+      systems = [ "x86_64-linux" "x86_64-linux" "aarch64-darwin" ];
+      perSystem = { pkgs, system, ... }:
+        let
+          inherit (nixpkgs) lib;
+          inherit (lib.strings)
+            concatMapStringsSep hasSuffix optionalString removePrefix;
+          inherit (lib.lists) optional;
 
-        # MacOS specific stuff
-        isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
-        frameworks = pkgs.darwin.apple_sdk.frameworks;
-        # Apple frameworks needed by the Notifications part of the tool
-        darwinInputs = with frameworks; [
-          Cocoa
-          Foundation
-          AppKit
-          CoreServices
-        ];
-        # Generate Linker flags for Apple Frameworks from the list of Framework packages
-        darwinLinkerFlags = strings.concatMapStringsSep " " (lib:
-          let libName = strings.removePrefix "apple-framework-" lib.pname;
-          in "-F${lib}/Library/Frameworks -framework ${libName}") darwinInputs;
+          naersk' = pkgs.callPackage naersk { };
 
-        nativeBuildInputs = lists.optional isDarwin darwinInputs;
-      in rec {
-        # `nix build`
-        packages.default = naersk-lib.buildPackage {
-          pname = "nixos-systool";
-          root = ./.;
+          # MacOS specific stuff
+          isDarwin = hasSuffix "-darwin" system;
+          frameworks = pkgs.darwin.apple_sdk.frameworks;
+          # Apple frameworks needed by the Notifications part of the tool
+          darwinInputs = with frameworks; [
+            Cocoa
+            Foundation
+            AppKit
+            CoreServices
+          ];
+          # Generate Linker flags for Apple Frameworks from the list of Framework packages
+          darwinLinkerFlags = concatMapStringsSep " " (lib:
+            let libName = removePrefix "apple-framework-" lib.pname;
+            in "-F${lib}/Library/Frameworks -framework ${libName}")
+            darwinInputs;
+          nativeBuildInputs = optional isDarwin darwinInputs;
 
-          inherit nativeBuildInputs;
-          NIX_LDFLAGS = strings.optionalString isDarwin darwinLinkerFlags;
-        };
-        # `nix run`
-        apps.default = utils.lib.mkApp { drv = packages.default; };
-        # `nix develop`
-        devShells.default = with pkgs;
-          mkShell {
-            nativeBuildInputs = nativeBuildInputs ++ [
+          # package definition
+          cargoToml = pkgs.lib.importTOML ./Cargo.toml;
+          pname = cargoToml.package.name;
+          package = naersk'.buildPackage {
+            inherit pname;
+            root = ./.;
+
+            inherit nativeBuildInputs;
+            NIX_LDFLAGS = optionalString isDarwin darwinLinkerFlags;
+          };
+        in {
+          # `nix build`
+          packages.default = package;
+          # `nix run`
+          apps.default.program = "${package}/bin/${pname}";
+          # `nix develop`
+          devshells.default = let
+            ansiClear = "[H[2J";
+            # Ansi 256 color code
+            rustColor = "{166}";
+          in {
+            motd = ''
+              ${ansiClear}${rustColor}{bold}🦀 Rust project - ${cargoToml.package.name} v${cargoToml.package.version}{reset}
+              This is the devshell for developing on this project. Use whatever editor
+              you're comfortable with to edit the code. The {166}{italic}rust-analyzer{reset} is
+              available for use.
+
+              Edit ${rustColor}{italic}flake.nix{reset} to change this greeting message.
+
+              This code is licensed under ${cargoToml.package.license} using Rust ${cargoToml.package.edition} edition.
+            '';
+            packages = nativeBuildInputs ++ (with pkgs; [
               rustc
               cargo
               cargo-deny
               cargo-outdated
+              cargo-readme
               clippy
               rustfmt
               rust-analyzer
+            ]);
+            env = [
+              {
+                name = "RUST_SRC_PATH";
+                value = pkgs.rustPlatform.rustLibSrc;
+              }
+              {
+                name = "NIX_LDFLAGS";
+                value = optionalString isDarwin darwinLinkerFlags;
+              }
             ];
-            RUST_SRC_PATH = rustPlatform.rustLibSrc;
-            NIX_LDFLAGS = strings.optionalString isDarwin darwinLinkerFlags;
           };
-      });
+        };
+    });
 }
