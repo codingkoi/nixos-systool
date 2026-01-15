5c8f49d091d994ec394870949dd869cffe08f0b7
Jeremy Shoemaker <jeremy@codingkoi.com>
2026-01-15 11:27:53 -0600

    Update project flake to use a different approach

--- flake.nix
+++ flake.nix
@@ -6,54 +6,54 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
     flake-parts.url = "github:hercules-ci/flake-parts";
+    rust-overlay.url = "github:oxalica/rust-overlay";
     devshell = {
       url = "github:numtide/devshell";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = inputs@{ self, nixpkgs, naersk, flake-parts, devshell, ... }:
-    flake-parts.lib.mkFlake { inherit inputs; } ({
-      imports = [ devshell.flakeModule ];
-      systems = [ "x86_64-linux" "x86_64-linux" "aarch64-darwin" ];
-      perSystem = { pkgs, system, ... }:
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake { inherit inputs; } ({
+      imports = [ inputs.devshell.flakeModule ];
+      systems = [ "x86_64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      perSystem = { config, self', pkgs, lib, system, ... }:
         let
-          inherit (nixpkgs) lib;
-          inherit (builtins) concatStringsSep map pathExists;
-          inherit (lib) readFile makeLibraryPath;
-          inherit (lib.strings)
-            concatMapStringsSep hasSuffix optionalString removePrefix;
-          inherit (lib.lists) optional;
+          runtimeDeps = with pkgs; [ ];
+          buildDeps = with pkgs; [ pkg-config rustPlatform.bindgenHook ];
+          devDeps = with pkgs;
+            [
+              #gdb # Not available on Darwin
+            ];
 
-          naersk' = pkgs.callPackage naersk { };
+          cargoToml = fromTOML (builtins.readFile ./Cargo.toml);
+          msrv = cargoToml.package.rust-version;
 
-          # MacOS specific stuff
-          isDarwin = hasSuffix "-darwin" system;
-          # Apple frameworks needed by the Notifications part of the tool
-          darwinInputs = [ pkgs.apple-sdk ];
-          nativeBuildInputs = optional isDarwin darwinInputs;
+          rustPackage = features:
+            (pkgs.makeRustPlatform {
+              cargo = pkgs.rust-bin.stable.latest.minimal;
+              rustc = pkgs.rust-bin.stable.latest.minimal;
+            }).buildRustPackage {
+              inherit (cargoToml.package) name version;
+              src = ./.;
+              cargoLock.lockFile = ./Cargo.lock;
+              buildFeatures = features;
+              buildInputs = runtimeDeps;
+              nativeBuildInputs = buildDeps;
+            };
 
-          # package definition
-          cargoToml = pkgs.lib.importTOML ./Cargo.toml;
-          pname = cargoToml.package.name;
-          package = naersk'.buildPackage {
-            inherit pname;
-            root = ./.;
-
-            inherit nativeBuildInputs;
-          };
-
-          # Rust toolchain version
-          toolchainToml = if (pathExists ./rust-toolchain.toml) then
-            pkgs.lib.importTOML ./rust-toolchain.toml
-          else {
-            toolchain = { channel = "stable"; };
-          };
         in {
+          _module.args.pkgs = import inputs.nixpkgs {
+            inherit system;
+            overlays = [ (import inputs.rust-overlay) ];
+          };
+
           # `nix build`
-          packages.default = package;
+          packages.default = self'.packages.nixos-systool;
+          packages.nixos-systool = rustPackage "";
           # `nix run`
-          apps.default.program = "${package}/bin/${pname}";
+          apps.default.program =
+            "${self'.packages.nixos-systool}/bin/${cargoToml.package.name}";
           # `nix develop`
           devshells.default = let
             # Ansi 256 color code for rust orange
@@ -71,49 +71,49 @@
               This code is licensed under ${cargoToml.package.license} using Rust ${cargoToml.package.edition} edition.
             '';
             packages = with pkgs; [
-              clang
-              rustup
+              rust-bin.stable.latest.default
+              rust-analyzer
               cargo-deny
               cargo-outdated
               cargo-readme
             ];
-            env = [
-              {
-                name = "RUSTC_VERSION";
-                value = toolchainToml.toolchain.channel;
-              }
-              {
-                name = "RUSTFLAGS";
-                value = concatStringsSep " " (map (a: "-L ${a}/lib") [ ]);
-              }
-              # Add glibc, clang, glib and other headers to bindgen search path
-              {
-                name = "BINDGEN_EXTRA_CLANG_ARGS";
-                # Includes with normal include path
-                value = concatStringsSep " "
-                  # Includes with special directory paths
-                  [
-                    ''
-                      -I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
-                    ''-I"${pkgs.glib.dev}/include/glib-2.0"''
-                    "-I${pkgs.glib.out}/lib/glib-2.0/include/"
-                  ];
-              }
-              {
-                name = "LIBCLANG_PATH";
-                value =
-                  makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
-              }
-              {
-                name = "RUST_SRC_PATH";
-                value = pkgs.rustPlatform.rustLibSrc;
-              }
-              {
-                name = "PATH";
-                eval =
-                  "$PATH:\${CARGO_HOME:~/.cargo}/bin:\${RUSTUP_HOME}:~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/";
-              }
-            ];
+            # env = [
+            #   {
+            #     name = "RUSTC_VERSION";
+            #     value = toolchainToml.toolchain.channel;
+            #   }
+            #   {
+            #     name = "RUSTFLAGS";
+            #     value = concatStringsSep " " (map (a: "-L ${a}/lib") [ ]);
+            #   }
+            #   # Add glibc, clang, glib and other headers to bindgen search path
+            #   {
+            #     name = "BINDGEN_EXTRA_CLANG_ARGS";
+            #     # Includes with normal include path
+            #     value = concatStringsSep " "
+            #       # Includes with special directory paths
+            #       [
+            #         ''
+            #           -I"${pkgs.llvmPackages_latest.libclang.lib}/lib/clang/${pkgs.llvmPackages_latest.libclang.version}/include"''
+            #         ''-I"${pkgs.glib.dev}/include/glib-2.0"''
+            #         "-I${pkgs.glib.out}/lib/glib-2.0/include/"
+            #       ];
+            #   }
+            #   {
+            #     name = "LIBCLANG_PATH";
+            #     value =
+            #       makeLibraryPath [ pkgs.llvmPackages_latest.libclang.lib ];
+            #   }
+            #   {
+            #     name = "RUST_SRC_PATH";
+            #     value = pkgs.rustPlatform.rustLibSrc;
+            #   }
+            #   {
+            #     name = "PATH";
+            #     eval =
+            #       "$PATH:\${CARGO_HOME:~/.cargo}/bin:\${RUSTUP_HOME}:~/.rustup}/toolchains/$RUSTC_VERSION-x86_64-unknown-linux-gnu/bin/";
+            #   }
+            # ];
           };
         };
     });
